f58635cf5d4a344d956e1a15eb147c995735102f
Andres Bonilla <abonilla@eventbrite.com>
2022-01-06 19:32:14 -0300

    Add tests for day 2

--- tests/aoc2021.rs
+++ tests/aoc2021.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 use aoc2021::day1::*;
+use aoc2021::day2::*;
 
 #[test]
 fn test_depth_increase() {
@@ -12,4 +13,18 @@ fn test_depth_increase() {
 fn test_sliding_window_increase() {
     let test_data = fs::read_to_string("day1-test.txt").unwrap();
     assert_eq!(get_sliding_window_increase(test_data.lines()), 5)
+}
+
+#[test]
+fn test_planned_course() {
+    let test_data = fs::read_to_string("day2-test.txt").unwrap();
+    let test_data = test_data.lines().collect();
+    assert_eq!(get_planned_course(&test_data), 150)
+}
+
+#[test]
+fn test_fixed_course() {
+    let test_data = fs::read_to_string("day2-test.txt").unwrap();
+    let test_data = test_data.lines().collect();
+    assert_eq!(get_fixed_course(&test_data), 900)
 }
