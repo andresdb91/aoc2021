8c2f165446e42dab0d8f09cc61a1f1b7900d8dbe
Andres Bonilla <abonilla@eventbrite.com>
2022-01-06 19:48:38 -0300

    Convert to (str, int) tuples

--- src/day2.rs
+++ src/day2.rs
@@ -1,4 +1,8 @@
 pub fn get_planned_course(data: &Vec<&str>) -> u32 {
+    let depth = data.iter().map(|x| {
+        let a: Vec<&str> = x.split(' ').collect();
+        (a[0], a[1].parse::<u32>().unwrap())
+    });
     1
 }
 
